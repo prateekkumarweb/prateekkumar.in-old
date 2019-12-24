602fc583c6c6df67221022d1f6358e8f2c1ad4f2
Prateek Kumar <prateek@prateekkumar.in>
2019-12-24 20:31:40 +0530

    Use buefy for components
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import Buefy from 'buefy';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
-import '../node_modules/bulma/css/bulma.css'
+import 'buefy/dist/buefy.css'
 import DefaultLayout from '~/layouts/Default.vue'
 
 config.autoAddCss = false
@@ -14,6 +15,10 @@ library.add(faGithub, faTwitter, faLinkedin, faEnvelope)
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout),
+  Vue.use(Buefy, {
+    defaultIconComponent: 'vue-fontawesome',
+    defaultIconPack: 'fas',
+  })
+  Vue.component('Layout', DefaultLayout)
   Vue.component('font-awesome', FontAwesomeIcon)
 }
