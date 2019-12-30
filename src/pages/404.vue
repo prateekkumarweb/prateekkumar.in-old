29f23cef1152348e940e02b8089b250b67a49c33
Prateek Kumar <prateek@prateekkumar.in>
2019-12-30 21:35:33 +0530

    Add 404 page
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

---
+++ src/pages/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <Section title="404 - Not Found">
+      <div class="m-12 lg:w-1/2">
+        <g-image src="/images/404.svg" class="w-full"></g-image>
+      </div>
+    </Section>
+  </Layout>
+</template>
+
+<script>
+import Section from "~/components/home/Section";
+
+export default {
+  metaInfo: {
+    title: "404 - Not Found | Prateek Kumar | Developer | Rust and JavaScript"
+  },
+  components: {
+    Section
+  }
+};
+</script>
