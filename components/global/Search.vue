ee2d699d9b4293894a7d2e0d8bfaeba6d681e99d
Prateek Kumar <prateek@prateekkumar.in>
2020-08-22 01:03:15 +0530

    Use fuse.js for search
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- components/global/Search.vue
+++ components/global/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="inline-block w-full md:w-64 relative">
     <div
-      class="rounded-lg p-1 text-gray-600 bg-white border border-transparent text-sm flex items-center w-full md:inline-block"
+      class="rounded-full py-1 px-2 text-gray-600 bg-white border border-transparent text-sm flex items-center w-full md:inline-block"
     >
       <font-awesome :icon="['fas', 'search']" class="mx-1"></font-awesome>
       <input
@@ -9,32 +9,65 @@
         type="text"
         class="focus:outline-none m-1 flex-grow"
         placeholder="Search"
-        @keyup.enter="search"
+        @keyup="debounce(search)"
       />
     </div>
     <div
       v-if="query"
-      class="absolute rounded-lg my-2 text-gray-600 bg-white border border-transparent text-sm w-full shadow-lg"
+      class="absolute rounded-lg my-2 text-gray-600 bg-white text-sm w-full shadow-lg z-10 overflow-hidden"
     >
-      <div class="p-3">No results available</div>
+      <div v-if="results.length === 0" class="p-3">No results available</div>
+      <div
+        v-for="(result, key) in results"
+        :key="result.refIndex"
+        class="p-3 hover:bg-blue-300 hover:text-primary"
+        :class="{ 'border-t': key !== 0 }"
+      >
+        <nuxt-link :to="getPostLink(result.item)">{{
+          result.item.title
+        }}</nuxt-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Fuse from 'fuse.js';
 
 export default Vue.extend({
   data() {
     return {
       query: '',
+      timeout: (null as unknown) as number,
+      results: [] as Fuse.FuseResult<Object>[],
     };
   },
   methods: {
-    search() {
-      // eslint-disable-next-line no-console
-      console.log(this.query);
-      this.query = '';
+    debounce(fn: Function, duration: number = 250) {
+      if (this.timeout !== null) {
+        window.clearTimeout(this.timeout);
+      }
+      this.timeout = window.setTimeout(() => {
+        fn();
+      }, duration);
+    },
+    async search() {
+      const posts = await this.$content('posts', { deep: true })
+        .sortBy('date', 'desc')
+        .fetch();
+      const fuse = new Fuse(posts, {
+        includeScore: true,
+        isCaseSensitive: false,
+        keys: ['title', 'tags'],
+      });
+      this.results = fuse.search(this.query);
+    },
+    getPostLink(post: any) {
+      const date = new Date(post.date);
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      return `/${year}/${month}/${post.slug}`;
     },
   },
 });
